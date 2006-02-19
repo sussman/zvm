5027586a487c1b76e443b8729dfc1b58251aa1ba
dave <dave@localhost>
2006-02-19 22:28:12 +0000

    Add a LICENSE file to the project tree, and repoint current source files to it.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,7 @@
+#
+# For the license of this file, please consult the LICENSE file in the
+# root directory of this distribution.
+#
+
 # All tests in the test suite.
 __all__ = [ "bitfield_tests" ]
---
+++ zvm/__init__.py
@@ -0,0 +1,4 @@
+#
+# For the license of this file, please consult the LICENSE file in the
+# root directory of this distribution.
+#
