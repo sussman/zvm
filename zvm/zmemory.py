ca675561405299ef4c06a4e652a16ccc6563043c
sussman <sussman@localhost>
2006-02-20 18:15:43 +0000

    * zvm/zmemory.py:  fix bugs, and add print_map() function.

--- zvm/zmemory.py
+++ zvm/zmemory.py
@@ -41,17 +41,18 @@ class ZMemory(object):
     self._total_size = len(initial_string)
     self._memory = [x for x in initial_string]
 
-    self._static_start = self.get_word_value(self._memory[0x0e])
+    self._static_start = self.get_word_value(0x0e)
     self._static_end = min(0x0ffff, self._total_size)
     self._dynamic_start = 0
     self._dynamic_end = self._static_start - 1
-    self._high_start = self.get_word_value(self._memory[0x04])
+    self._high_start = self.get_word_value(0x04)
     self._high_end = self._total_size
 
   ### implement sequence funcs:
   ### http://python.org/doc/2.4.2/ref/sequence-types.html
 
   def _bytes_to_16bit_int(self, byte1, byte2):
+    """Convert two bytes into a 16-bit integer."""
     return (ord(byte1) << 8) + ord(byte2)
 
   def get_word_value(self, index):
@@ -60,6 +61,12 @@ class ZMemory(object):
       raise ZMemoryOutOfBounds
     return self._bytes_to_16bit_int(self._memory[index],
                                     self._memory[(index + 1)])
+
+  def print_map(self):
+    """Pretty-print a description of the memory map."""
+    print "Dynamic memory: ", self._dynamic_start, "-", self._dynamic_end
+    print " Static memory: ", self._static_start, "-", self._static_end
+    print "   High memory: ", self._high_start, "-", self._high_end
 
   def __getitem__(self, index):
     """Return the byte value stored at address INDEX.."""
