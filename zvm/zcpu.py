4d25b4ca038c5d8e578387119108018d210cb77e
dave <dave@localhost>
2006-03-24 23:36:02 +0000

    Add versionning support for opcodes.

--- zvm/zcpu.py
+++ zvm/zcpu.py
@@ -6,11 +6,23 @@
 # root directory of this distribution.
 #
 
+from zopdecoder import ZOpDecoder
+
 class ZCpuError(Exception):
     "General exception for Zcpu class"
 
+class ZCpuOpcodeOverlap(ZCpuError):
+    "Overlapping opcodes registered"
+
 class ZCpuIllegalInstruction(ZCpuError):
     "Illegal instruction encountered"
+
+def declare_opcode(func, optype, opcode, version=(1,2,3,4,5)):
+    """Helper function used for declaring functions implementing
+    opcodes."""
+    func._optype = optype
+    func._opcode = opcode
+    func._opversion = version
 
 class ZCpu(object):
 
@@ -22,17 +34,24 @@ class ZCpu(object):
 
         print self._opcodes
 
-    def _get_handler(self, opcode):
+    def _get_handler(self, optype, opcode):
         try:
-            return getattr(self, self._opcodes[opcode])
+            print "Opcode key:", (optype, opcode, self._memory.version)
+            return getattr(self, self._opcodes[(optype, opcode,
+                                                self._memory.version)])
         except KeyError:
+            print "Unknown instruction (%s, 0x%X)" % (optype, opcode)
             raise ZCpuIllegalInstruction
 
     def run(self):
         while True:
-            (type, opcode, operands) = self._opdecoder.get_next_instruction()
-            self._get_handler(opcode)(*operands)
+            (optype, opcode, operands) = self._opdecoder.get_next_instruction()
+            self._get_handler(optype, opcode)(*operands)
 
+    def op_add(a, b):
+        """16-bit signed addition"""
+        print a,'+',b,'=',a+b
+    declare_opcode(op_add, "2OP", 0x14)
 
     # This is the "automagic" opcode handler registration system.
     # After each function that is an opcode handler, we assign the
@@ -47,5 +66,11 @@ class ZCpu(object):
     # retrieve the function implementing a given opcode.  Pretty cool
     # voodoo if you ask me.
     for k,v in vars().items():
-        if hasattr(v, "_opcode"):
-            _opcodes[v._opcode] = k
+        if hasattr(v, "_optype"):
+            for ver in v._opversion:
+                opkey = (v._optype, v._opcode, ver)
+
+                if opkey in _opcodes.keys():
+                    raise ZCpuOpcodeOverlap
+
+                _opcodes[opkey] = k
