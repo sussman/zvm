8b73f0e4e70fa1ac6705a4c44878f4910beb8cfb
dave <dave@localhost>
2006-04-19 20:02:49 +0000

    Revert r67, which was not the changeset intended for commit.

--- tests/scratchtest2.py
+++ tests/scratchtest2.py
@@ -18,22 +18,4 @@ print "   word separators are", lexer._separators
 print "   each dict value is", lexer.get_dictionary_entry_length(lexer._dict_addr), "bytes long"
 print "   there are", lexer.get_dictionary_num_entries(lexer._dict_addr), "entries in the dictionary"
 
-dict = lexer.get_dictionary(lexer._dict_addr)
-print dict
-
-print
-
-print "dictionary has", len(dict.keys()), "items"
-
 print lexer._dict
-
-def lex_split(str, separators):
-    split_str = []
-    prev_i = 0
-    i = 0
-    while i < len(str):
-        if str[i] in separators:
-            split_str.append(str[prev_i:i])
-            split_str.append(str[i])
-            prev_i = i+1
-        i = i+1
