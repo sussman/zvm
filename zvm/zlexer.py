6c229d4e4497d6e3e6812160fcc6c2f148eff2bd
sussman <sussman@localhost>
2007-04-18 01:04:43 +0000

    Finish the tokenisation and parsing class!
    * zvm/zlexer.py:  rewrite this whole file to be leaner and meaner.
                      finish all tokenisation/parsing functionality.
    
    * tests/lexer_tests.py:  new unit tests to test dictionary loading,
                             tokenisation and parsing
    
    * tests/scratchtest2.py:  remove old cruft.
    
    * README:  mention docs/

--- zvm/zlexer.py
+++ zvm/zlexer.py
@@ -1,16 +1,28 @@
 #
 # A class for parsing word dictionaries and performing lexical
-# analysis of user input.
+# analysis of user input.  (See section 13 of the z-machine spec.)
 #
 # For the license of this file, please consult the LICENSE file in the
 # root directory of this distribution.
 #
 
+import re
 from zstring import ZStringFactory, ZsciiTranslator
 
 class ZLexerError(Exception):
   "General exception for ZLexer class"
 
+# Note that the specification describes tokenisation as a process
+# whereby the user's input is divided into words, each word converted
+# to a z-string, then searched for in the 'standard' dictionary.  This
+# is really inefficient.  Therefore, because the standard dictionary
+# is immutable (lives in static memory), this class parses and loads
+# it *once* into a private python dictionary.  We can then forever do
+# O(1) lookups of unicode words, rather than O(N) lookups of
+# zscii-encoded words.
+
+# Note that the main API here (tokenise_input()) can work with any
+# dictionary, not just the standard one.
 
 class ZLexer(object):
 
@@ -20,63 +32,62 @@ class ZLexer(object):
     self._stringfactory = ZStringFactory(self._memory)
     self._zsciitranslator = ZsciiTranslator(self._memory)
 
-    # Load the game's 'standard' dictionary into a python
-    # dictionary.  It's safe to do this, because the dictionary
-    # exists in static memory; it's a read-only thing.  So let's
-    # only parse it ONCE, at start-up.  :-)
-    self._dict_addr = self._memory.read_word(0x08)
-    self._dict = self.get_dictionary(self._dict_addr)
-    self._separators = self.get_dictionary_word_separators (self._dict_addr)
+    # Load and parse game's 'standard' dictionary from static memory.
+    dict_addr = self._memory.read_word(0x08)
+    self._num_entries, self._entry_length, self._separators, entries_addr = \
+                       self._parse_dict_header(dict_addr)
+    self._dict = self.get_dictionary(dict_addr)
 
-  def _separators_to_ascii_string(self, separators):
-    """Convert a list of zscii separator codes into an ascii string."""
 
-    string = ""
-    for code in separators:
-      string += self._zsciitranslator.ztou(code)
-    return string
-
-  def get_dictionary_word_separators(self, address):
-    """Return the list of zscii-codes listed as word separators for
-    the dictionary at ADDRESS. """
+  def _parse_dict_header(self, address):
+    """Parse the header of the dictionary at ADDRESS.  Return the
+    number of entries, the length of each entry, a list of zscii
+    word separators, and an address of the beginning the entries."""
 
     addr = address
     num_separators = self._memory[addr]
-    return self._memory[(addr+1):(addr+num_separators)]
-
-  def get_dictionary_num_entries(self, address):
-    """Return the number of entries in the dictionary at ADDRESS."""
-
-    addr = address
-    num_separators = self._memory[addr]
-    addr += (2 + num_separators)
-    return self._memory.read_word(addr)
-
-  def get_dictionary_entry_length(self, address):
-    """Return the length of each bytestring value in the dictionary at
-    ADDRESS.  """
-
-    addr = address
-    num_separators = self._memory[addr]
-    addr += (1 + num_separators)
-    return self._memory[addr]
-
-  def get_dictionary(self, address):
-    """Load a z-machine-format dictionary at ADDRESS -- which maps
-    zstrings to bytestrings -- into a python dictionary which maps
-    ascii strings to the address of the word in the original
-    dictionary.  Return the new dictionary."""
-
-    dict = {}
-
-    # read the header
-    addr = address
-    num_separators = self._memory[addr]
+    separators = self._memory[(addr + 1):(addr + num_separators)]
     addr += (1 + num_separators)
     entry_length = self._memory[addr]
     addr += 1
     num_entries = self._memory.read_word(addr)
     addr += 2
+
+    return num_entries, entry_length, separators, addr
+
+
+  def _tokenise_string(self, string, separators):
+     """Split unicode STRING into a list of words, and return the list.
+    Whitespace always counts as a word separator, but so do any
+    unicode characters provided in the list of SEPARATORS.  Note,
+    however, that instances of these separators caunt as words
+    themselves."""
+
+     # re.findall(r'[,.;]|\w+', 'abc, def')
+     sep_string = ""
+     for sep in separators:
+       sep_string += sep
+     if sep_string == "":
+       regex = r"\w+"
+     else:
+       regex = r"[%s]|\w+" % sep_string
+
+     return re.findall(regex, string)
+
+
+  #--------- Public APIs -----------
+
+
+  def get_dictionary(self, address):
+    """Load a z-machine-format dictionary at ADDRESS -- which maps
+    zstrings to bytestrings -- into a python dictionary which maps
+    unicode strings to the address of the word in the original
+    dictionary.  Return the new dictionary."""
+
+    dict = {}
+
+    num_entries, entry_length, separators, addr = \
+                 self._parse_dict_header(address)
 
     for i in range(0, num_entries):
       text_key = self._stringfactory.get(addr)
@@ -85,41 +96,45 @@ class ZLexer(object):
 
     return dict
 
-  def tokenise_input(self, text_addr, len, dict_addr=None):
-    """Given an ascii string at zmemory address TEXT_ADDR of length
-    LEN bytes, perform lexical analysis.
+
+  def parse_input(self, string, dict_addr=None):
+    """Given a unicode string, parse it into words based on a dictionary.
 
     if DICT_ADDR is provided, use the custom dictionary at that
     address to do the analysis, otherwise default to using the game's
     'standard' dictionary.
 
+    The dictionary plays two roles: first, it specifies separator
+    characters beyond the usual space character.  Second, we need to
+    look up each word in the dictionary and return the address.
+
     Return a list of lists, each list being of the form
 
-       [word,
-        byte_address_of_word_in_dictionary (or 0 if not in dictionary),
-        number_of_letters_in_word,
-        address_of_first_letter_of_word]
+       [word, byte_address_of_word_in_dictionary (or 0 if not in dictionary)]
     """
 
-    main_list = []
-    unicode_input = "" ### convert text_addr/len zcodes to unicode string?
-
     if dict_addr is None:
+      zseparators = self._separators
       dict = self._dict
-      separators = self._separators
     else:
+      num_entries, entry_length, zseparators, addr = \
+                   self._parse_dict_header(dict_addr)
       dict = self.get_dictionary(dict_addr)
-      separators = self.get_dictionary_word_separators(dict_addr)
 
-    ### assume we get a tokenized list here somehow, splitting the
-    ### string on space and other pre-defined separators.
+    # Our list of word separators are actually zscii codes that must
+    # be converted to unicode before we can use them.
+    separators = []
+    for code in zseparators:
+      separators.append(self._zsciitranslator.ztou(code))
 
+    token_list = self._tokenise_string(string, separators)
+
+    final_list = []
     for word in token_list:
       if dict.has_key(word):
         byte_addr = dict[word]
       else:
         byte_addr = 0
-      word_location = text_addr + unicode_input.find(word)
-      main_list.append([word, byte_addr, len(word), word_location])
+      final_list.append([word, byte_addr])
 
-    return main_list
+    return final_list
