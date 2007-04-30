db50bf421b109663c6e0eb7b628708c08c285f16
david.jc.anderson <david.jc.anderson@localhost>
2007-04-30 02:05:45 +0000

    Refactor the ZMemory initialization into the unit test's setUp method, so that loading the lexer module has no side-effects.

--- tests/lexer_tests.py
+++ tests/lexer_tests.py
@@ -8,25 +8,25 @@ from unittest import TestCase
 from zvm.zmemory import ZMemory
 from zvm.zlexer import ZLexer
 
-# We use Graham Nelson's 'curses' game for our unittests.
-storydata = file("stories/curses.z5").read()
-mem = ZMemory(storydata)
-
 phrase1 = u"the quick brown fox"
 phrase2 = u"the quick, brown,;fox\t might,,be   fe;el.ing ;; odd, .today"
 phrase3 = u"the fish, house, and bird are odd and round, no?"
 
 class ZLexerTests(TestCase):
+  def setUp(self):
+    # We use Graham Nelson's 'curses' game for our unittests.
+    storydata = file("stories/curses.z5").read()
+    self.mem = ZMemory(storydata)
 
   def testParseDictionary(self):
     # load game's 'standard' dictionary into a python dictionary
-    lexer = ZLexer(mem)
+    lexer = ZLexer(self.mem)
     num_keys = len(lexer._dict.keys())
     assert num_keys == lexer._num_entries, \
            "lexer didn't parse all dictionary entries"
 
   def testTokenisation(self):
-    lexer = ZLexer(mem)
+    lexer = ZLexer(self.mem)
     separators = (u',', u';', u'.')
 
     tokens = lexer._tokenise_string(phrase1, separators)
@@ -38,7 +38,7 @@ class ZLexerTests(TestCase):
                       ';', ';', 'odd', ',', '.', 'today']
 
   def testParseInput(self):
-    lexer = ZLexer(mem)
+    lexer = ZLexer(self.mem)
 
     results = lexer.parse_input(phrase3)
     assert results == [[u'the', 40278], [u'fish', 33150], [u',', 0], \
